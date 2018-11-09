55ac1ad6b0b294e8c6e3139098399c8c74fb49c1
Federica Amato <fede9423@live.it>
2018-11-09 12:47:19 +0100

    Refactor name

--- src/test/java/it/ariadne/BookingManagement/TestBooking.java
+++ src/test/java/it/ariadne/BookingManagement/TestBooking.java
@@ -4,15 +4,15 @@ import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
-import it.ariadne.BookingManagement.resorces.Proiettore;
+import it.ariadne.BookingManagement.resorces.Projector;
 
 public class TestBooking {
 	@Test
 	public void testBookingRequest() {
 		// Assert first
-		Resource res = new Proiettore();
+		Resource res = new Projector();
 		boolean bookingreq = res.bookingRequest();
-		Resource res1 = new Proiettore();
+		Resource res1 = new Projector();
 		boolean bookingreq1 = res1.bookingRequest();
 		assertEquals("Se una risorsa è occupata la richiesta torna falso", false, bookingreq);
 		assertEquals("Se una risorsa è libera la richiesta torna vero", true, bookingreq1);
