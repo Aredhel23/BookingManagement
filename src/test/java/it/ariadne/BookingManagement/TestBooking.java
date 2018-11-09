44ed7a17bb1a1570df7ecec2e40771707390b056
Federica Amato <fede9423@live.it>
2018-11-09 12:28:08 +0100

    Added TestClass with the first test

---
+++ src/test/java/it/ariadne/BookingManagement/TestBooking.java
@@ -0,0 +1,20 @@
+package it.ariadne.BookingManagement;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import it.ariadne.BookingManagement.resorces.Proiettore;
+
+public class TestBooking {
+	@Test
+	public void testBookingRequest() {
+		// Assert first
+		Resource res = new Proiettore();
+		boolean bookingreq = res.bookingRequest();
+		Resource res1 = new Proiettore();
+		boolean bookingreq1 = res1.bookingRequest();
+		assertEquals("Se una risorsa è occupata la richiesta torna falso", false, bookingreq);
+		assertEquals("Se una risorsa è libera la richiesta torna vero", true, bookingreq1);
+	}
+}
