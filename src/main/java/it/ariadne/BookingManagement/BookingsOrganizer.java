359173543609da4f53eaf726dd18a9b836626ef4
Federica Amato <fede9423@live.it>
2018-11-09 17:57:05 +0100

    WIP: bookingRequest

--- src/main/java/it/ariadne/BookingManagement/BookingsOrganizer.java
+++ src/main/java/it/ariadne/BookingManagement/BookingsOrganizer.java
@@ -1,5 +1,32 @@
 package it.ariadne.BookingManagement;
 
-public class BookingsOrganizer {
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
+import org.joda.time.DateTime;
+
+public class BookingsOrganizer {
+	private Map<Resource, Booking> organizer= new LinkedHashMap<>();
+
+	public Map<Resource, Booking> getOrganizer() {
+		return organizer;
+	}
+	
+	public boolean bookingRequest(Resource r, DateTime start, DateTime end) {
+		Iterator it = organizer.entrySet().iterator();
+		 while (it.hasNext()) {
+			    Map.Entry entry = (Map.Entry)it.next();
+			    if(entry.getKey().equals(r)) {
+			    	Booking bookings = (Booking) entry.getValue();
+			    	
+			    }
+		 }
+		  
+		return false;
+	}
+
+	
+	
 }
+
