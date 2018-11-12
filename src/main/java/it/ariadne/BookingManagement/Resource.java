e1b5dbdd8ceed2590980172523b7c0e193e032fc
Federica Amato <fede9423@live.it>
2018-11-12 09:57:41 +0100

    Deleted methods that are under responsibility of bookings

--- src/main/java/it/ariadne/BookingManagement/Resource.java
+++ src/main/java/it/ariadne/BookingManagement/Resource.java
@@ -5,19 +5,5 @@ import org.joda.time.DateTime;
 import org.joda.time.Interval;
 
 public interface Resource {
-	 static boolean bookingRequest(List<Booking> list, DateTime start, DateTime end) {
-		Interval interval = new Interval(start, end);
-		for (Booking i:list) {
-			if(i.getInterval().overlaps(interval)) {
-				return false;
-			}
-		}
-		return true;
-		
-	}
-
-	boolean bookingRequest(DateTime start1, DateTime end1);;
 	public List<Booking> getList();
-	int addBooking(String name, DateTime start, DateTime end);
-	int deleteBooking(String string, DateTime start, DateTime end);
 }
--- src/main/java/it/ariadne/BookingManagement/resorces/Projector.java
+++ src/main/java/it/ariadne/BookingManagement/resorces/Projector.java
@@ -9,45 +9,20 @@ import it.ariadne.BookingManagement.Booking;
 import it.ariadne.BookingManagement.Resource;
 
 public class Projector implements Resource {
+	
 	private List<Booking> projectorList;
 	
 	public Projector(List<Booking> l) {
 		this.projectorList = l;
 	}
 	
-	@Override
-	public boolean bookingRequest(DateTime start, DateTime end) {
-		return Resource.bookingRequest(projectorList, start, end);
-	}
-	
+
 	@Override
 	public List<Booking> getList() {
 		return projectorList;
 	}
 
-	@Override
-	public int addBooking(String name, DateTime start, DateTime end) {
-		if(bookingRequest(start, end)) {
-			Interval interval = new Interval(start, end);
-			Booking p = new Booking(name, interval);
-			projectorList.add(p);
-			return 0;
-		}
-		else 
-			return 1;
-	}
 
-	@Override
-	public int deleteBooking(String name, DateTime start, DateTime end) {
-		for (Booking b:projectorList) {
-			if(b.getName().equals(name))
-				if(b.getI().getStart().equals(start) && b.getI().getEnd().equals(end)) {
-					projectorList.remove(b);
-					return 0;
-				}
-		}
-		return 1;
-	}
 
 
 
