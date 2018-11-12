1103a1b396f3c1c99f016046c58c6de4a2a38c7b
Federica Amato <fede9423@live.it>
2018-11-12 09:56:50 +0100

    Added deleted booking

--- src/main/java/it/ariadne/BookingManagement/BookingsOrganizer.java
+++ src/main/java/it/ariadne/BookingManagement/BookingsOrganizer.java
@@ -62,6 +62,24 @@ public class BookingsOrganizer {
 		 return 2;
 	}
 	
-}
+	public int deleteBooking(Resource r, String name, DateTime start, DateTime end) {
+		Iterator it = organizer.entrySet().iterator();
+		 while (it.hasNext()) {
+			    Map.Entry entry = (Map.Entry)it.next();
+			    if(entry.getKey().equals(r)) {
+			    	List<Booking> bookings = (ArrayList)entry.getValue();
+					for (Booking b:bookings) {
+						if(b.getName().equals(name))
+							if(b.getI().getStart().equals(start) && b.getI().getEnd().equals(end)) {
+								bookings.remove(b);
+								return 0;
+							}
+					}
+					return 1;
+			    }
+		 }
+		 return 2;
 	}
+}
+
 
