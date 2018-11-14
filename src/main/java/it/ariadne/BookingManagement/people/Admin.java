825091e9dfb89dc5ae43da7d50928cffd4b5d6f3
Federica Amato <fede9423@live.it>
2018-11-14 15:46:49 +0100

    Added method to print by resource

--- src/main/java/it/ariadne/BookingManagement/people/Admin.java
+++ src/main/java/it/ariadne/BookingManagement/people/Admin.java
@@ -1,9 +1,15 @@
 package it.ariadne.BookingManagement.people;
 
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
+
+import org.joda.time.DateTime;
 
 import it.ariadne.BookingManagement.BookingsOrganizer;
 import it.ariadne.BookingManagement.Person;
+import it.ariadne.BookingManagement.Resource;
 
 public class Admin extends Person {
 
@@ -11,12 +17,25 @@ public class Admin extends Person {
 		super(name, surname, email, password);
 	}
 
-	public Object userBookings(BookingsOrganizer ba, List<User> users) {
+	public String userBookings(BookingsOrganizer ba, List<User> users) {
 		String s = "";
 		for (User us: users) {
 			s = s + us.myBookings(ba);
 		}
 		return s;
 	}
+
+	public String resourceBookings(BookingsOrganizer ba) {
+		Iterator it = ba.getOrganizer().entrySet().iterator();
+		String s ="";
+		 while (it.hasNext()) {
+			    Map.Entry entry = (Map.Entry)it.next();
+			    Resource r = (Resource)entry.getKey();
+			    s = s + r.bookingPrintList();
+		 }
+		return s;
+	}
+	
+	
 
 }
