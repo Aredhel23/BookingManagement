efe53383961206675304c6fb765fd682f6c69f5c
Federica Amato <fede9423@live.it>
2018-11-16 17:03:30 +0100

    Added javadocs of the class Person and its methods

--- src/main/java/it/ariadne/BookingManagement/Person.java
+++ src/main/java/it/ariadne/BookingManagement/Person.java
@@ -6,12 +6,23 @@ import java.util.List;
 import java.util.Map;
 
 import org.joda.time.DateTime;
-
+/***
+ * Abstract Class that picks general attributes of a real person.
+ * @author amato
+ *
+ */
 public abstract class Person {
 	private String name;
 	private String surname;
 	private String email;
 	private String password;
+	/***
+	 * Person constructor.
+	 * @param name
+	 * @param surname
+	 * @param email
+	 * @param password
+	 */
 	public Person(String name, String surname, String email, String password) {
 		this.name = name;
 		this.surname = surname;
@@ -24,6 +35,12 @@ public abstract class Person {
 		return s;
 	}
 	
+	/***
+	 * method to print the list of the booking of a Person.
+	 * 
+	 * @param ba: an instance of the booking organizer where are the bookings. 
+	 * @return the String listing of all the bookings of the Person.
+	 */
 	public String myBookings(BookingsOrganizer ba) {
 		String s = this.toString() + "\n";
 		Iterator it = ba.getOrganizer().entrySet().iterator();
@@ -38,6 +55,11 @@ public abstract class Person {
 		}
 		return s;
 	}
+	/***
+	 * method to generate the string containing the bookings done for the future.
+	 * @param ba: an instance of the booking organizer where are the bookings.
+	 * @return The String listing of all the future bookings of the Person.
+	 */
 	public String myFutureBookings(BookingsOrganizer ba) {
 		String s = this.toString() + "\n";
 		Iterator it = ba.getOrganizer().entrySet().iterator();
@@ -53,6 +75,12 @@ public abstract class Person {
 		}
 		return s;
 	}
+	
+	/***
+	 * method to generate the string containing the bookings done in the past.
+	 * @param ba: an instance of the booking organizer where are the bookings.
+	 * @return The String listing of all the past bookings of the Person.
+	 */
 	public String myPastBookings(BookingsOrganizer ba) {
 
 		String s = this.toString() + "\n";
