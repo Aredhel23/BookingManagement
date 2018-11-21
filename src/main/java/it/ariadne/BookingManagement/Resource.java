3f886e79b6a1f5bf066dbe45ca21f619024d7f96
Federica Amato <fede9423@live.it>
2018-11-21 11:26:19 +0100

    Added resource docstrings

--- src/main/java/it/ariadne/BookingManagement/Resource.java
+++ src/main/java/it/ariadne/BookingManagement/Resource.java
@@ -5,9 +5,25 @@ import org.joda.time.DateTime;
 import org.joda.time.Interval;
 
 public interface Resource {
+	/***
+	 * it gets the list of the booking of the resource.
+	 * @return the list of the booking of the resource.
+	 */
 	public List<Booking> getList();
+	/***
+	 * it gets the type of the resource.
+	 * @return the type of the resource.
+	 */
 	public String getType();
+	/***
+	 * it gets the limit of the resource.
+	 * @return gets the limit of the resource.
+	 */
 	public int getLimit();
+	/***
+	 * it prints the list of all the bookings of the resource. 
+	 * @return the String of all the bookings of the resource. 
+	 */
 	public String bookingPrintList();
 	
 }
