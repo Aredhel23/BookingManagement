9f3c7dce9f7524e82724df38b00a8932bee467b6
Federica Amato <fede9423@live.it>
2018-11-21 10:55:16 +0100

    Added docstrings to booking

--- src/main/java/it/ariadne/BookingManagement/Booking.java
+++ src/main/java/it/ariadne/BookingManagement/Booking.java
@@ -3,35 +3,56 @@ package it.ariadne.BookingManagement;
 import org.joda.time.Interval;
 import org.joda.time.ReadableInterval;
 import org.joda.time.base.AbstractInterval;
-
+/***
+ * class that represent the single booking
+ * @author amato
+ *
+ */
 public class Booking {
 	private Interval i;
 	private String name;
 	private Person u;
-	
-	public Person getUser() {
-		return u;
-	}
 
-
-
+	/***
+	 * Constructor of the booking.
+	 * @param u person that makes the booking.
+	 * @param name name of the booking.
+	 * @param i interval where there is the booking.
+	 */
 	public Booking(Person u, String name, Interval i) {
 		this.name = name;
 		this.i = i;
 		this.u = u;
 	}
+	
+	/***
+	 * Gets the Person that makes the booking.
+	 * @return the Person that makes the booking.
+	 */
+	public Person getUser() {
+		return u;
+	}
 
-
+	/***
+	 * It gets the interval where there is the booking.
+	 * @return the interval where there is the booking.
+	 */
 	ReadableInterval getInterval() {
 		return i;
 	}
 
-
+	/***
+	 * It gets the interval where there is the booking.
+	 * @return the interval where there is the booking.
+	 */
 	public Interval getI() {
 		return i;
 	}
 
-
+	/***
+	 * It gets the name of the booking.
+	 * @return the name of the booking.
+	 */
 	public String getName() {
 		return name;
 	}
