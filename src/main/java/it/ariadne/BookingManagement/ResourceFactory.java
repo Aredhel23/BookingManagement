03cfb0371a84cfcefccc5844d05c85f9795385cb
Federica Amato <fede9423@live.it>
2018-12-11 16:39:59 +0100

    Added javadoc

--- src/main/java/it/ariadne/BookingManagement/ResourceFactory.java
+++ src/main/java/it/ariadne/BookingManagement/ResourceFactory.java
@@ -4,7 +4,11 @@ import it.ariadne.BookingManagement.resources.Car;
 import it.ariadne.BookingManagement.resources.Projector;
 
 public class ResourceFactory {
-	//use getShape method to get object of type shape 
+	/***
+	 * method to create a resource
+	 * @param ResourceType (type of resource you want)
+	 * @return the resource if is present
+	 */
 	   public Resource getResource(String ResourceType){
 	      if(ResourceType == null){
 	         return null;
